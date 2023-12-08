a114e79302872b70c646984b858c63d2056a66aa
Megan Hokama <you@example.com>
2023-12-07 19:48:44 -0700

    q4 finished

--- app1/app.R
+++ app1/app.R
@@ -1,6 +1,4 @@
 # Load packages ----------------------------------------------------------------
-install.packages("fastmap")
-install.packages("Rtools")
 library(shiny)
 library(tidyverse)
 library(ggplot2)
@@ -8,58 +6,61 @@ library(fastmap)
 
 # Load data --------------------------------------------------------------------
 
-life_meaning_app <- read_csv("app1/data/life_meaning.csv", show_col_types = FALSE)
+life_meaning_app <- read_csv("data/life_meaning.csv", show_col_types = FALSE)
 
-#Pivot longer 
+# Pivot longer 
 life_meaning_long_app <- tidyr::gather(life_meaning_app, key = "Choice_Order", value = "Choice", -country)
+
+#Shift Order
+order <- c("First_Choice", "Second_Choice", "Third_Choice", "Fourth_Choice", "Fifth_Choice")
+
+# Pivot longer 
+life_meaning_long_app$Choice_Order <- factor(life_meaning_long_app$Choice_Order, levels = order)
+
 
 # Define UI --------------------------------------------------------------------
 
 ui <- fluidPage(
   titlePanel("Sources of meaning trends by country"),
-  HTML("<i>Ranked choice among 17 topics coded as part of what gives people meaning in life</i>"),
+  HTML("<i>Choose a country</i>"),
   sidebarLayout(
     sidebarPanel(
-      
-      #Select variable type of trend to plot
-      selectInput("countryInput", "Select a country to highlight:", choices = unique(life_meaning$country), selected = "Japan")
+      # Country Input panel
+      selectInput("countryInput", "Select Country", choices = unique(life_meaning_long_app$country)),
     ),
+    
+    # Output plot
     mainPanel(
-      plotOutput("countryPlot")
+      plotOutput("plot")
     )
-  )
+  ) 
 )
 
 # Define server function --------------------------------------------
 server <- function(input, output) {
+  
   # Create a reactive subset of the data for the selected country
   filtered_data <- reactive({
-    life_meaning_app %>%
+    life_meaning_long_app %>%
       filter(country == input$countryInput)
   })
   
   # Reactive plot based on the selected country
-  g <- ggplot(life_meaning_long_app, aes(x = Choice, fill = country))
-  g+geom_bar(aes(fill=country), width = .5) +
-    scale_fill_viridis_d() +
-    labs(title = "Total Frequency of Life Meaning Topics by Country",
-         x = "Choice Category",
-         y = "Total Frequency",
-         fill = "Country") +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, vjust = .6))+
-    theme(legend.position = "right",
-          plot.title = element_text(hjust = .5, face = "bold"))
- 
+  output$plot <- renderPlot({
+    ggplot(filtered_data(), aes(x = Choice, y = Choice_Order, fill = country)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      scale_fill_viridis_d () + 
+      labs(title = paste("Total Frequency of Life Meaning Topics for", input$countryInput),
+           x = "Choice Category",
+           y = "Rank",
+           fill = "Country") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, vjust = .6)) +
+      theme(legend.position = "right",
+            plot.title = element_text(hjust = .5, face = "bold"))
   })
-}
 }
 
 # Create the Shiny app object ---------------------------------------
 shinyApp(ui = ui, server = server)
 
-#Installing shinyapps.io
-install.packages('rsconnect')
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='t1p1qw-megan0marguerite-hokama', token='94867C5188A9800086A3B56A581EF376', secret='+9hwU5NFO91IGDaTXxs1Vs/gURVSoDB3C9cK8I7/')
